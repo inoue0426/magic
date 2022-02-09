fc65245051f408a6d96300845755a421d485ad3a
inoue0426 <sekihan_tennis@yahoo.co.jp>
2022-02-09 12:38:24 -0600

    modify magic_core

--- src/magic/MAGIC_core.py
+++ src/magic/MAGIC_core.py
@@ -2,25 +2,30 @@ import numpy as np
 import pandas as pd
 from scipy.sparse import issparse, csr_matrix, find
 from sklearn.metrics.pairwise import pairwise_distances
-from sklearn.manifold.t_sne import _joint_probabilities, _joint_probabilities_nn
+from sklearn.manifold._t_sne import _joint_probabilities, _joint_probabilities_nn
 from sklearn.decomposition import PCA
 from scipy.spatial.distance import squareform
 from sklearn.neighbors import NearestNeighbors
 
-def magic(data, n_pca_components=20, random_pca=True, 
-          t=6, k=30, ka=10, epsilon=1, rescale=99):
+
+def magic(
+    data, n_pca_components=20, random_pca=True, t=6, k=30, ka=10, epsilon=1, rescale=99
+):
 
     if n_pca_components != None:
-        print('doing PCA')
-        pca_projected_data = run_pca(data, n_components=n_pca_components, random=random_pca)
+        print("doing PCA")
+        pca_projected_data = run_pca(
+            data, n_components=n_pca_components, random=random_pca
+        )
     else:
         pca_projected_data = data
 
-    #run diffusion maps to get markov matrix
-    L = compute_markov(pca_projected_data, k=k, epsilon=epsilon, 
-                       distance_metric='euclidean', ka=ka)
+    # run diffusion maps to get markov matrix
+    L = compute_markov(
+        pca_projected_data, k=k, epsilon=epsilon, distance_metric="euclidean", ka=ka
+    )
 
-    #remove tsne kernel for now
+    # remove tsne kernel for now
     # else:
     #     distances = pairwise_distances(pca_projected_data, squared=True)
     #     if k_knn > 0:
@@ -33,7 +38,7 @@ def magic(data, n_pca_components=20, random_pca=True,
     #     #markov normalize P
     #     L = np.divide(P, np.sum(P, axis=1))
 
-    #get imputed data matrix -- by default use data_norm but give user option to pick
+    # get imputed data matrix -- by default use data_norm but give user option to pick
     new_data, L_t = impute_fast(data, L, t, rescale_percent=rescale)
 
     return new_data
@@ -41,9 +46,9 @@ def magic(data, n_pca_components=20, random_pca=True,
 
 def run_pca(data, n_components=100, random=True):
 
-    solver = 'randomized'
+    solver = "randomized"
     if random != True:
-        solver = 'full'
+        solver = "full"
 
     pca = PCA(n_components=n_components, svd_solver=solver)
     return pca.fit_transform(data)
@@ -51,27 +56,27 @@ def run_pca(data, n_components=100, random=True):
 
 def impute_fast(data, L, t, rescale_percent=0, L_t=None, tprev=None):
 
-    #convert L to full matrix
+    # convert L to full matrix
     if issparse(L):
         L = L.todense()
 
-    #L^t
-    print('MAGIC: L_t = L^t')
+    # L^t
     if L_t == None:
         L_t = np.linalg.matrix_power(L, t)
     else:
-        L_t = np.dot(L_t, np.linalg.matrix_power(L, t-tprev))
+        L_t = np.dot(L_t, np.linalg.matrix_power(L, t - tprev))
 
-    print('MAGIC: data_new = L_t * data')
     data_new = np.array(np.dot(L_t, data))
 
-    #rescale data
+    # rescale data
     if rescale_percent != 0:
         if len(np.where(data_new < 0)[0]) > 0:
-            print('Rescaling should not be performed on log-transformed '
-                  '(or other negative) values. Imputed data returned unscaled.')
+            print(
+                "Rescaling should not be performed on log-transformed "
+                "(or other negative) values. Imputed data returned unscaled."
+            )
             return data_new, L_t
-            
+
         M99 = np.percentile(data, rescale_percent, axis=0)
         M100 = data.max(axis=0)
         indices = np.where(M99 == 0)[0]
@@ -82,21 +87,20 @@ def impute_fast(data, L, t, rescale_percent=0, L_t=None, tprev=None):
         M99_new[indices] = M100_new[indices]
         max_ratio = np.divide(M99, M99_new)
         data_new = np.multiply(data_new, np.tile(max_ratio, (len(data), 1)))
-    
+
     return data_new, L_t
 
 
-def compute_markov(data, k=10, epsilon=1, distance_metric='euclidean', ka=0):
+def compute_markov(data, k=10, epsilon=1, distance_metric="euclidean", ka=0):
 
     N = data.shape[0]
 
     # Nearest neighbors
-    print('Computing distances')
+    print("Computing distances")
     nbrs = NearestNeighbors(n_neighbors=k, metric=distance_metric).fit(data)
     distances, indices = nbrs.kneighbors(data)
-
     if ka > 0:
-        print('Autotuning distances')
+        print("Autotuning distances")
         for j in reversed(range(N)):
             temp = sorted(distances[j])
             lMaxTempIdxs = min(ka, len(temp))
@@ -106,7 +110,7 @@ def compute_markov(data, k=10, epsilon=1, distance_metric='euclidean', ka=0):
                 distances[j] = np.divide(distances[j], temp[lMaxTempIdxs])
 
     # Adjacency matrix
-    print('Computing kernel')
+    print("Computing kernel")
     rows = np.zeros(N * k, dtype=np.int32)
     cols = np.zeros(N * k, dtype=np.int32)
     dists = np.zeros(N * k)
@@ -118,10 +122,9 @@ def compute_markov(data, k=10, epsilon=1, distance_metric='euclidean', ka=0):
         dists[inds] = distances[i, :]
         location += k
     if epsilon > 0:
-        W = csr_matrix( (dists, (rows, cols)), shape=[N, N] )
+        W = csr_matrix((dists, (rows, cols)), shape=[N, N])
     else:
-        W = csr_matrix( (np.ones(dists.shape), (rows, cols)), shape=[N, N] )
-
+        W = csr_matrix((np.ones(dists.shape), (rows, cols)), shape=[N, N])
     # Symmetrize W
     W = W + W.T
 
@@ -130,14 +133,14 @@ def compute_markov(data, k=10, epsilon=1, distance_metric='euclidean', ka=0):
         rows, cols, dists = find(W)
         rows = np.append(rows, range(N))
         cols = np.append(cols, range(N))
-        dists = np.append(dists/(epsilon ** 2), np.zeros(N))
-        W = csr_matrix( (np.exp(-dists), (rows, cols)), shape=[N, N] )
+        dists = np.append(dists / (epsilon ** 2), np.zeros(N))
+        W = csr_matrix((np.exp(-dists), (rows, cols)), shape=[N, N])
 
     # Create D
-    D = np.ravel(W.sum(axis = 1))
-    D[D!=0] = 1/D[D!=0]
+    D = np.ravel(W.sum(axis=1))
+    D[D != 0] = 1 / D[D != 0]
 
-    #markov normalization
+    # markov normalization
     T = csr_matrix((D, (range(N), range(N))), shape=[N, N]).dot(W)
 
     return T
@@ -152,4 +155,3 @@ def optimal_t(data, th=0.001):
         S_t = np.power(S, t)
         P = np.divide(S_t, np.sum(S_t, axis=0))
         nse[t] = np.sum(P[np.where(P > th)[0]])
-
